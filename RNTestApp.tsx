edd3aa84c75f8cf15c032316b90e20a2ae67d8d3
arjun-tare <arjun.tare@ishafoundation.org>
2023-03-08 15:55:43 +0530

    RNTestApp.tsx

---
+++ RNTestApp.tsx
@@ -0,0 +1,15 @@
+import {Text, View} from 'react-native';
+
+export const RNTestApp = () => {
+  return (
+    <View
+      style={{
+        backgroundColor: 'grey',
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Text>{'RNTestApp'}</Text>
+    </View>
+  );
+};
